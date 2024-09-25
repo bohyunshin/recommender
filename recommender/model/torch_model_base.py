6f5dd7b088db641991e4961bdc6495be5dd37571
bohyun-shin <bohyun.shin@linecorp.com>
2024-09-25 20:58:45 +0900

    refactor by parent class

---
+++ recommender/model/torch_model_base.py
@@ -0,0 +1,17 @@
+from abc import abstractmethod
+from torch import nn
+
+from model.recommender_base import RecommenderBase
+
+
+class TorchModelBase(nn.Module, RecommenderBase):
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def predict(self, user_factors, item_factors):
+        raise NotImplementedError
+
+    @abstractmethod
+    def forward(self):
+        raise NotImplementedError
