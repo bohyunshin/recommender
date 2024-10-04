603d01324c8e51b3bae411a34594b317a5b21ebf
bohyun-shin <bohyun.shin@linecorp.com>
2024-10-04 21:25:23 +0900

    add svd bias model

--- recommender/loss/custom.py
+++ recommender/loss/custom.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
 
@@ -8,3 +9,11 @@ def bpr_loss(pred, params, regularization):
     for param in params:
         penalty = penalty + param.data.norm(dim=1).pow(2).sum() * regularization
     return -logprob + penalty
+
+
+def svd_loss(pred, true, params, regularization):
+    mse = F.mse_loss(pred, true, reduction='mean')
+    penalty = torch.tensor(0., requires_grad=True)
+    for param in params:
+        penalty = penalty + param.data.norm(dim=1).pow(2).sum() * regularization
+    return mse + penalty
