414b3f2d2618e99668ae3ff88d198b8dfd1fedde
bohyun-shin <bohyun.shin@linecorp.com>
2024-10-07 21:29:10 +0900

    add generalized mf

---
+++ recommender/model/deep_learning/gmf.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from model.torch_model_base import TorchModelBase
+
+class Model(TorchModelBase):
+    def __init__(self, num_users, num_items, num_factors):
+        super(Model, self).__init__()
+
+        self.embed_user = nn.Embedding(num_users, num_factors)
+        self.embed_item = nn.Embedding(num_items, num_factors)
+
+        nn.init.xavier_normal_(self.embed_user.weight)
+        nn.init.xavier_normal_(self.embed_item.weight)
+
+    def forward(self, user_idx, item_idx):
+        return F.sigmoid(self.embed_user[user_idx] * self.embed_item[item_idx])
