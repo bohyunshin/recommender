afc06e89d16ff56fe9296e392bccd265c9bbec0a
bohyun-shin <bohyun.shin@linecorp.com>
2024-10-12 17:30:32 +0900

    add pytest file for train_csr.py

---
+++ tests/test_train_csr.py
@@ -0,0 +1,30 @@
+import argparse
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),"../recommender"))
+
+from train_csr import main
+
+def run_csr_model(model, dataset):
+    args = argparse.ArgumentParser()
+    args.dataset = dataset
+    args.model = model
+    args.implicit = True
+    args.batch_size = 32
+    args.regularization = 1e4
+    args.lr = 1e2
+    args.epochs = 1
+    args.num_factors = 16
+    args.train_ratio = 0.8
+    args.random_state = 42
+    args.patience = 5
+    args.log_path = f"{model}.log"
+    args.model_path = f"{model}.pkl"
+    args.num_sim_user_top_N = 45
+    args.movielens_data_type = "ml-1m"
+
+    main(args)
+
+
+def test_als():
+    run_csr_model("als", "movielens")
