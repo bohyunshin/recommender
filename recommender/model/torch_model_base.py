3ed5a3934decabe626b6c728e9301fad8aed5ddd
bohyun-shin <bohyun.shin@linecorp.com>
2024-11-20 08:17:33 +0900

    fix predict method in bpr

--- recommender/model/torch_model_base.py
+++ recommender/model/torch_model_base.py
@@ -9,23 +9,6 @@ class TorchModelBase(nn.Module, RecommenderBase):
     def __init__(self):
         super().__init__()
 
-    @abstractmethod
-    def predict(self, user_factors, item_factors, userid, **kwargs):
-        """
-        Calculate user-item scores based on learned user / item embeddings
-
-        Parameters
-        ----------
-        user_factors : Tensor (M1 x K)
-
-        item_factors : Tensor (N1 x K)
-
-        Returns
-        -------
-        user_item_scores : Tensor (M1 x N1)
-        """
-        return np.dot(user_factors[userid], item_factors.T)
-
     def set_trained_embedding(self):
         self.user_factors = self.embed_user.weight.data.clone().detach().cpu().numpy()
         self.item_factors = self.embed_item.weight.data.clone().detach().cpu().numpy()
