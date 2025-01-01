5c44728abb9f5a5697c1fe00846a37ecc0402848
bohyun-shin <bohyun.shin@linecorp.com>
2025-01-02 07:33:16 +0900

    feat: delete unused example file

--- examples/movielens.py
+++
@@ -1,7 +0,0 @@
-from model.mf.als import AlternatingLeastSquares
-from datasets.movielens import get_movielens
-
-if __name__ == '__main__':
-    titles, ratings = get_movielens(variant="1m")
-    als = AlternatingLeastSquares()
-    als.fit(ratings)
