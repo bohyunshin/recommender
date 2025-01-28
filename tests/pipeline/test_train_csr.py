bc89485c562b09b01db2ed525858eee9ab1d571d
bohyun-shin <bohyun.shin@linecorp.com>
2025-01-28 22:46:02 +0900

    test: update train_csr pipeline test using pytest.mark.parametrize

--- tests/pipeline/test_train_csr.py
+++ tests/pipeline/test_train_csr.py
@@ -1,38 +1,15 @@
-import argparse
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)),"../"))
+import pytest
 
 from recommender.train_csr import main
 
-def run_csr_model(
-        model: str,
-        dataset: str
-):
-    args = argparse.ArgumentParser()
-    args.dataset = dataset
-    args.model = model
-    args.implicit = True
-    args.loss = "not_defined"
-    args.num_neg = 1
-    args.batch_size = 32
-    args.regularization = 1e-4
-    args.lr = 1e-2
-    args.epochs = 10
-    args.num_factors = 16
-    args.train_ratio = 0.8
-    args.random_state = 42
-    args.patience = 5
-    args.result_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), f"../result/{model}")
-    args.num_sim_user_top_N = 45
-    args.test = True
-    args.num_sim_user_top_N = 5
 
-    main(args)
-
-
-def test_als():
-    run_csr_model("als", "movielens")
-
-def test_user_based():
-    run_csr_model("user_based", "movielens")
+@pytest.mark.parametrize(
+    "setup_config",
+    [
+        ("movielens", "als", "als", False, None, []),
+        ("movielens", "user_based", "not_defined", False, None, []),
+    ],
+    indirect=["setup_config"],
+)
+def test_train_csr(setup_config):
+    main(setup_config)
